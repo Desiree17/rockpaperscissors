5b37bf77d4bf2809824f35908143a11bb07a387f
Desiree Betts <anibetts10@gmail.com>
2018-11-14 23:14:20 +0000

    rock_paper_scissor_start

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
+$("#shoot").click(function(){
+    var randomNumber =Math.random();
+    
+    if(randomNumber > .6666666666666) {
+        $("#computerChoice").text("rock!"); 
+    } else if(randomNumber >.3333333333333 && randomNumber < .6666666666666){
+        $("#computerChoice").text("paper!"); 
+    } else if(randomNumber > .0000000000000 && randomNumber < .3333333333333){
+        $("#computerChoice").text("scissor!"); 
+    }
+    });
 
 
 // DOCUMENT READY FUNCTION BELOW
